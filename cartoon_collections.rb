ebd4d53dc799d87dde24e64009f4edffe625c4be
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-19 15:12:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
-  {planeteer_calls.find do |string|} ==
-    {valid_calls.include? }
+  planeteer_calls.find {|string| string == valid_calls.include?} 
   
  end    
 end 
